b026ba3ab58089c7b7fd6439823bee2d480df7db
Mason Fisher <sackzombie10@gmail.com>
2021-03-09 19:18:01 -0500

    Made properties menu

--- javascript/calc.js
+++ javascript/calc.js
@@ -1,6 +1,6 @@
 function calculate() {
     var total_damage = document.getElementById('total_damage');
-    var weapon_material = document.getElementById('weapon_material');
+    var weapon_material = document.getElementById('weapon_material_select');
     var weapon_type = document.getElementById('weapon_type');
     var damage = 0;
 
@@ -46,10 +46,22 @@ function material_switch(material, wood_gold, stone, iron, diamond, nether) {
 }
 
 function updateType(type) {
-    var material_select = document.getElementById('weapon_material');
+    var weapon_material = document.getElementById('weapon_material');
+    var weapon_type = document.getElementById('weapon_type')
+    var weapon_name = document.getElementById('weapon_name');
+    var material_name = weapon_type.options[weapon_type.selectedIndex].text;
+    weapon_name.innerHTML = material_name + " properties";
     if (type == 'sword' | type == 'axe') {
-        material_select.disabled = false;
+        weapon_material.style.display = 'block';
     } else {
-        material_select.disabled = true;
+        weapon_material.style.display = 'none';
     }
+}
+
+function init() {
+  var material_select = document.getElementById('weapon_material');
+  var weapon_type = document.getElementById('weapon_type');
+  material_select.value = 'wood';
+  material_select.style.display = 'none';
+  weapon_type.value = 'fist';
 }
